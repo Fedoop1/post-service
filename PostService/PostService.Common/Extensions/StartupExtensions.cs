b33bdf25bc2d56d29ab6ae48a397b459fd485178
Malukov Nikita <nikitaminsk833@gmail.com>
2022-05-22 14:46:21 +0300

    Add startup initializer extension method

--- PostService/PostService.Common/Extensions/StartupExtensions.cs
+++ PostService/PostService.Common/Extensions/StartupExtensions.cs
@@ -1,7 +1,32 @@
-﻿namespace PostService.Common.Extensions;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PostService.Common.Types;
+
+namespace PostService.Common.Extensions;
 public static class StartupExtensions
 {
-   
+    public static void AddStartupInitializer(this WebApplicationBuilder webBuilder, params Type[] initializers)
+    {
+        if (!initializers.Any()) return;
 
-   
+        webBuilder.Services.AddTransient<IStartupInitializer, StartupInitializer>(provider =>
+        {
+            var startupInitializer = new StartupInitializer();
+            foreach (var initializer in initializers.Where(initializer => typeof(IInitializer).IsAssignableFrom(initializer)))
+            {
+                var initializationProvider = provider.GetService(initializer) as IInitializer;
+                startupInitializer.AddInitializer(initializationProvider);
+            }
+
+            return startupInitializer;
+        });
+
+    }
+
+
+    public static async void InitializeAsync(this WebApplication webApplication)
+    {
+        var startupInitializer = webApplication.Services.GetService<IStartupInitializer>();
+        await startupInitializer.InitializeAsync();
+    }
 }
