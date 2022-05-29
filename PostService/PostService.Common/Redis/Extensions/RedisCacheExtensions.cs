c07960ef62e451381e26c83bcaa5bcd5d2ed3e9e
Nikita Malukov <nikita_malukov@epam.com>
2022-05-29 15:45:12 +0300

    Add Redis extension logic

---
+++ PostService/PostService.Common/Redis/Extensions/RedisCacheExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using PostService.Common.Redis.Types;
+using StackExchange.Redis;
+
+namespace PostService.Common.Redis.Extensions;
+
+public static class RedisCacheExtensions
+{
+    public const string SectionName = "Redis";
+
+    public static void AddRedis(this WebApplicationBuilder webBuilder)
+    {
+        ConfigureRedisCache(webBuilder);
+
+        using var providers = webBuilder.Services.BuildServiceProvider();
+        var redisOptions = providers.GetService<IOptions<RedisCacheOptions>>().Value;
+
+        webBuilder.Services.AddDistributedRedisCache(config =>
+        {
+            config.InstanceName = redisOptions.Instance;
+            config.ConfigurationOptions = new ConfigurationOptions()
+            {
+                EndPoints = {redisOptions.ConnectionString},
+                Password = redisOptions.Password,
+                ClientName = redisOptions.ClientName,
+            };
+        });
+    }
+
+    private static void ConfigureRedisCache(this WebApplicationBuilder webBuilder) =>
+        webBuilder.Services.AddOptions<RedisCacheOptions>().BindConfiguration(SectionName);
+}
---
+++ PostService/PostService.Common/Redis/Types/RedisCacheOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace PostService.Common.Redis.Types;
+public class RedisCacheOptions
+{
+    public string ConnectionString { get; set; }
+    public string Instance { get; set; }
+    public string ClientName { get; set; }
+    public string Password { get; set; }
+}
+
