be6235c9e70c9f143124dc80296d9153db16c771
Nikita Malukov <nikita_malukov@epam.com>
2022-06-12 01:01:08 +0300

    Add type name serializer for EasyNetQ library

---
+++ PostService/PostService.Common/RabbitMq/Types/TypeNameSerializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using EasyNetQ;
+using IMessage = PostService.Common.Types.IMessage;
+
+namespace PostService.Common.RabbitMq.Types;
+
+internal class TypeNameSerializer : ITypeNameSerializer
+{
+    public string Serialize(Type type) => type.Name;
+
+    public Type DeSerialize(string typeName) => (Assembly.GetEntryAssembly()?.DefinedTypes
+            .Where(type => type.IsClass && type.IsAssignableTo(typeof(IMessage))) ?? Array.Empty<TypeInfo>())
+        .FirstOrDefault(message => message.Name.Equals(typeName)) ?? throw new RabbitMqExtensionException(
+            $"Message with type {typeName} doesn't exist in {Assembly.GetEntryAssembly()?.GetName().Name} assembly");
+
+
+}
