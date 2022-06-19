cbf0d97556a036205a1c25eba2bc89b8c50e4c12
Nikita Malukov <nikita_malukov@epam.com>
2022-06-20 00:07:12 +0300

    Add operations contoller in operations project

---
+++ PostService/PostService.Operations/Controllers/OperationsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostService.Operations.Services;
+
+namespace PostService.Operations.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class OperationsController : ControllerBase
+{
+    private readonly IOperationStorage operationStorage;
+
+    public OperationsController(IOperationStorage operationStorage)
+    {
+        this.operationStorage = operationStorage;
+    }
+
+    [HttpGet("{operationId:guid}")]
+    public async Task<IActionResult> Get(Guid operationId)
+    {
+        var result = await this.operationStorage.GetAsync(operationId);
+        return result is not null ? Ok(result) : NotFound();
+    }
+}
