9a07ac131f7d7cc5f5fd322c6478ed8702cf8dce
Nikita Malukov <nikita_malukov@epam.com>
2022-06-20 00:06:53 +0300

    Add template for RestEase extension logic

---
+++ PostService/PostService.Common/RestEase/Types/RestEaseOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace PostService.Common.RestEase.Types;
+
+public record RestEaseOptions
+{
+    public string LoadBalancer { get; set; }
+    public IEnumerable<Service> Services { get; set; }
+}
---
+++ PostService/PostService.Common/RestEase/Types/Service.cs
@@ -0,0 +1,9 @@
+﻿namespace PostService.Common.RestEase.Types;
+
+public record Service
+{
+    public string Name { get; set; }
+    public string Scheme { get; set; }
+    public string Host { get; set; }
+    public string Port { get; set; }
+}
