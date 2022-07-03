26a261f79aa9551b4522971b6ec889d7e2b704d6
Nikita Malukov <nikita_malukov@epam.com>
2022-07-03 15:07:24 +0300

    Add tracing extension

---
+++ PostService/PostService.Common/Tracing/Extensions/TracingExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PostService.Common.Tracing.Middlewares;
+
+namespace PostService.Common.Tracing.Extensions;
+
+public static class TracingExtensions
+{
+    public static void AddTracing(this WebApplicationBuilder webBuilder) =>
+        webBuilder.Services.AddScoped<TraceRequestModifierMiddleware>();
+
+    public static void UseTracing(this WebApplication webApplication) =>
+        webApplication.UseMiddleware<TraceRequestModifierMiddleware>();
+}
---
+++ PostService/PostService.Common/Tracing/Middlewares/TraceRequestModifierMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using PostService.Common.Constants;
+
+namespace PostService.Common.Tracing.Middlewares;
+
+public class TraceRequestModifierMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        if (string.IsNullOrEmpty(context.Request.Headers[Headers.TraceHeader]))
+        {
+            context.Request.Headers[Headers.TraceHeader] = Guid.NewGuid().ToString("N");
+        }
+
+        await next(context);
+
+        if (!string.IsNullOrEmpty(context.Request.Headers[Headers.TraceHeader]))
+        {
+            context.Request.Headers[Headers.TraceHeader] = string.Empty;
+        }
+    }
+}
+
